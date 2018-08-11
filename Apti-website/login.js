1baf211710afe4023c9e039a6f96e3121c6736d9
Dev Indurkar <devindurkar84@gmail.com>
2018-08-12 00:10:31 +0530

    removal of php and linking with firebase

--- Apti-website/login.js
+++ Apti-website/login.js
@@ -26,9 +26,38 @@ function signo(){
 }
 
 function questionbank(){
-  var bankname = document.getElementById("bankbtn1").innerHTML;
-  var fireref = firebase.database().ref("https://try1-d007.firebaseio.com/" + bankname);
-  
+  console.log("hey");
+  var i;
+  for(i=1;i<=10;i++){
+    var text = "q" + i;
+    document.getElementById(text).style.display = "block";
+  }
+  document.getElementById("startbtn").style.display = "none";
+
+  var fireref = firebase.database().ref("Bank1");
+  i = 0;
+    fireref.once("value",function(snap){
+      snap.forEach(function(childsnap) {
+        var childKey = childsnap.key;
+        console.log(childKey);
+        var qt = document.getElementById(childKey);
+        var t_head = qt.tHead;
+        var t_body = document.getElementsByTagName("tbody")[i];
+        i++;
+        t_head.children[0].children[0].innerHTML = snap.child(childKey+"/Question").val();
+        t_body.rows[0].cells[1].innerHTML = snap.child(childKey+"/Options/0").val();
+        t_body.rows[1].cells[1].innerHTML = snap.child(childKey+"/Options/1").val();
+        t_body.rows[2].cells[1].innerHTML = snap.child(childKey+"/Options/2").val();
+        t_body.rows[3].cells[1].innerHTML = snap.child(childKey+"/Options/3").val();
+      });
+
+    });
+
+}
+
+for(i=1;i<=10;i++){
+  var text = "q" + i;
+  document.getElementById(text).style.display = "none";
 }
 
 firebase.auth().onAuthStateChanged(function(user) {
